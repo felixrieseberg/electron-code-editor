6afe4ded46b0abd9fc6f625cb32982c87f7a8b96
Felix Rieseberg <felix@felixrieseberg.com>
2016-11-18 05:21:48 -0800

    :wrench: Simple code editor

--- renderer.js
+++ renderer.js
@@ -1,3 +1,21 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+
+const loader = require('./node_modules/monaco-editor/min/vs/loader.js')
+
+loader.require.config({
+  baseUrl: encodeURI(`file://${__dirname}/node_modules/monaco-editor/min`)
+})
+
+// Help Monaco understand how there's both Node and Browser stuff
+self.module = undefined
+self.process.browser = true
+
+loader.require(['vs/editor/editor.main'], () => {
+  let editor = monaco.editor.create(document.getElementById('container'), {
+    language: 'javascript',
+    theme: 'vs-dark',
+    automaticLayout: true
+  })
+})
