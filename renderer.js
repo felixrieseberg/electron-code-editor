8807fffaed3b236338e2649d92a2ef98a6284882
Felix Rieseberg <felix@felixrieseberg.com>
2017-01-26 22:27:59 +0100

    :wrench: A Simple code editor

--- renderer.js
+++ renderer.js
@@ -1,3 +1,9 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// All of the Node.js APIs are available in this process.
+const loader = require('monaco-loader')
+
+loader().then(monaco => {
+  let editor = monaco.editor.create(document.getElementById('container'), {
+    language: 'javascript',
+    theme: 'vs-dark',
+    automaticLayout: true
+  })
+})
